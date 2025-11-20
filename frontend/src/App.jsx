e010944150a180923de162cc89208ae7bc0faeb4
BRID19 <74292639+BRID19@users.noreply.github.com>
2025-11-20 13:48:36 +0100

    Created Landing page for SafeTalk app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import Navbar from "./components/Navbar.jsx"
+import Hero from "./components/Hero.jsx"
+import FeaturesSection from "./components/FeaturesSection.jsx"
+import StepsCard from "./components/StepsCard.jsx"
+import CTA from "./components/CTA.jsx"
+import ContactUs from "./components/ContactUs.jsx"
+import Footer from "./components/Footer.jsx"
 
 function App() {
   return (
     <div>
-    
+      <Navbar />
+      <Hero />
+      <FeaturesSection />
+      <StepsCard />
+      <CTA />
+      <ContactUs />
+      <Footer />
     </div>
   )
 }
